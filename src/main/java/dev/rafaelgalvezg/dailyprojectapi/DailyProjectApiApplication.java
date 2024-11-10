e9e73534a2fa5a3b7415b4346af9f9dbfe475999
rafaelGalvezG <rafael.galvezg@gmail.com>
2024-11-10 09:29:40 -0600

    initial commit

---
+++ src/main/java/dev/rafaelgalvezg/dailyprojectapi/DailyProjectApiApplication.java
@@ -0,0 +1,12 @@
+package dev.rafaelgalvezg.dailyprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyProjectApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyProjectApiApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/rafaelgalvezg/dailyprojectapi/DailyProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rafaelgalvezg.dailyprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyProjectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
