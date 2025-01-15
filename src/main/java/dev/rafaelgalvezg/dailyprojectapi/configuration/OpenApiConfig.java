2b18916db3aaed72ffc46c79ff6298f65873fda9
rafaelGalvezG <rafael.galvezg@gmail.com>
2025-01-15 14:41:15 -0600

    add option token swagger ui

--- src/main/java/dev/rafaelgalvezg/dailyprojectapi/configuration/OpenApiConfig.java
+++ src/main/java/dev/rafaelgalvezg/dailyprojectapi/configuration/OpenApiConfig.java
@@ -1,20 +1,30 @@
 package dev.rafaelgalvezg.dailyprojectapi.configuration;
 
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("API DAILY PROJECT")
                         .version("1.0.0")
-                        .description("Documentation of the API for the Daily Project application"));
+                        .description("Documentation of the API for the Daily Project application"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
-
-
 }
